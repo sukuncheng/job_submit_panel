db6a133827217afd16c0a257ace8c505cc9a4743
sukuncheng <sukun.cheng@nersc.no>
2020-06-22 11:30:25 +0200

    22-6-4

--- 2020DA_scripts/part2_core.sh
+++ 2020DA_scripts/part2_core.sh
@@ -1,35 +1,35 @@
 #!/bin/bash
-# #------ PART 1: ensemble forecast ------------
-# echo "ensemble runs start"
-# for (( mem=1; mem<=${ESIZE}; mem++ )); do
-#     cd ${ENSPATH}/${ENSEMBLE[${mem}]}  #MEMPATH
-#     # update time_init per time step
-#     sed -i "s;^time_init=.*$;time_init="${time_init}";g" ./nextsim.cfg
-#     sed -i "s;^restart_from_analysis=.*$;restart_from_analysis="${restart_from_analysis}";g" ./nextsim.cfg 
+#------ PART 1: ensemble forecast ------------
+echo "ensemble runs start"
+for (( mem=1; mem<=${ESIZE}; mem++ )); do
+    cd ${ENSPATH}/${ENSEMBLE[${mem}]}  #MEMPATH
+    # update time_init per time step
+    sed -i "s;^time_init=.*$;time_init="${time_init}";g" ./nextsim.cfg
+    sed -i "s;^restart_from_analysis=.*$;restart_from_analysis="${restart_from_analysis}";g" ./nextsim.cfg 
    
-#     # submit job        
-#     source $ENVFRAM/run.fram.sh ./nextsim.cfg 1 -e $ENVFRAM/nextsim.src       
-#     # barrier of max instants
-#     job_list=$(squeue -u chengsukun)
-#     XPID=$(grep -o chengsuk <<<$job_list |wc -l)  # number of current running jobs
-#     echo $XPID    
-#     while [[ $XPID -ge $maximum_instants ]]; do # maximum of running instants
-#         sleep 20
-#         job_list=$(squeue -u chengsukun)
-#         XPID=$(grep -o chengsuk <<<$job_list |wc -l)  # number of current running jobs
-#     done        
-# done # ensemble loop
-# # wait for finish of all ensemble simulations
-# while [[ $XPID -ge 1 ]]; do #
-#     sleep 20
-#     job_list=$(squeue -u chengsukun)
-#     XPID=$(grep -o chengsuk <<<$job_list |wc -l)  # number of current running jobs
-# done  
-# echo "ensemble forecast done" 
-# #    . run.fram.sh $cfg 1 -e ~/nextsim.ensemble.src                  
-# #    1 - copy nextsim.exec from NEXTSIMDIR/model/bin to current path
-# #   -t test run without submit to fram
-# #   -e ~/nextsim.ensemble.src      # envirmonental variables
+    # submit job        
+    source $ENVFRAM/run.fram.sh ./nextsim.cfg 1 -e $ENVFRAM/nextsim.src       
+    # barrier of max instants
+    job_list=$(squeue -u chengsukun)
+    XPID=$(grep -o chengsuk <<<$job_list |wc -l)  # number of current running jobs
+    echo $XPID    
+    while [[ $XPID -ge $maximum_instants ]]; do # maximum of running instants
+        sleep 20
+        job_list=$(squeue -u chengsukun)
+        XPID=$(grep -o chengsuk <<<$job_list |wc -l)  # number of current running jobs
+    done        
+done # ensemble loop
+# wait for finish of all ensemble simulations
+while [[ $XPID -ge 1 ]]; do #
+    sleep 20
+    job_list=$(squeue -u chengsukun)
+    XPID=$(grep -o chengsuk <<<$job_list |wc -l)  # number of current running jobs
+done  
+echo "ensemble forecast done" 
+#    . run.fram.sh $cfg 1 -e ~/nextsim.ensemble.src                  
+#    1 - copy nextsim.exec from NEXTSIMDIR/model/bin to current path
+#   -t test run without submit to fram
+#   -e ~/nextsim.ensemble.src      # envirmonental variables
 
 
 #-------  PART 2: enkf - UPDATE ENSEMBLE  ---------
