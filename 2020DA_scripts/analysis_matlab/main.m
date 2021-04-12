6fc25521233f75d6dd84026d8eb1941b1bcc4bd7
Sukun Cheng <sukun.cheng@nersc.no>
2021-04-12 13:46:34 +0200

    add gif of SIC and SIT

--- 2020DA_scripts/analysis_matlab/main.m
+++ 2020DA_scripts/analysis_matlab/main.m
@@ -6,28 +6,35 @@ function [] = main()
     % mnt_dir = '/Users/sukeng/Desktop/nird'; % mac;  %  mnt_dir = 'Z:\';  % window  
     % mnt_dir = '/Users/sukeng/Desktop/fram';
     mnt_dir='/cluster/work/users/chengsukun/simulations'; 
-    mnt_dir='/nird/projects/nird/NS2993K/NORSTORE_OSL_DISK/NS2993K/chengsukun';
+    % mnt_dir='/nird/projects/nird/NS2993K/NORSTORE_OSL_DISK/NS2993K/chengsukun';
     %%
-    start_date = "2019-09-03"; N_periods = 1; Duration = 42;
-    for i = 1:Duration
-        dates(i) = datetime(start_date) + i;    
-        dates_num(i) = datenum(dates(i));
-    end
-    simul_dir = '/test_windcohesion_2019-09-03_42days_x_1cycles_memsize40';
-    %%
-    % start_date = "2019-10-15";
-    % N_periods = 12;
-    % Duration = 7; % duration days set in nextsim.cfg 
-    % for i = 1:N_periods
-    %     dates(i) = datetime(start_date) + (i-1)*Duration +1;    
+    % Exp_ID='Exp_03Sep2019'
+    % start_date = "2019-09-03"; N_periods = 1; Duration = 42;
+    % for i = 1:Duration
+    %     dates(i) = datetime(start_date) + i;    
     %     dates_num(i) = datenum(dates(i));
     % end
-    % simul_dir = '/test_windcohesion_2019-10-15_7days_x_12cycles_memsize40';
-    
+    % simul_dir = '/test_windcohesion_2019-09-03_42days_x_1cycles_memsize40';
+
+    %%
+    Exp_ID='Exp_15Oct2019';
+    start_date = "2019-10-15";
+    N_periods = 12;
+    Duration = 7; % duration days set in nextsim.cfg 
+    n = 0;
+    for i = 1:N_periods
+        for j = 1:Duration
+            n = n + 1;
+            dates(n) = datetime(start_date) + n - 1;    
+        end
+        dates_num = datenum(dates);
+    end
+    simul_dir = '/test_windcohesion_2019-10-15_7days_x_12cycles_memsize40';
+
+    % ----------------------------------
     simul_dir = [mnt_dir simul_dir];   
-    save('test_inform.mat','dates','N_periods','dates_num','Ne','simul_dir','Duration','Radius')
+    save('test_inform.mat','Exp_ID','dates','N_periods','dates_num','Ne','simul_dir','Duration','Radius')
 %%
 %     main_ensemblesize
-    main_moorings_spinup
-%     main_moorings   % plot temporal average and spatial averages of spread of sit 
+    main_moorings_animation   
 %     main_observation_RCRV
---
+++ 2020DA_scripts/analysis_matlab/main_moorings_animation.m
@@ -0,0 +1,129 @@
+function [] = main_moorings_spinup()
+    clc
+    clear
+    close all
+    dbstop if error
+    
+    for Var = {'SIC','SIT'}
+        for method = {'Spread','mean'}  %mean: ensemble ensemble, spread: ensemble spread
+            fun_moorings(char(Var),char(method));
+        end
+    end
+end
+
+function fun_moorings(Var,method);
+    load('test_inform.mat')   % 
+    check_a_member = 0; % check_a_member=0 presents ensemble average
+%% ------------------------------------------------------------------------ 
+    figure(1); set(gcf,'color','w')
+    m_proj('Stereographic','lon',-45,'lat',90,'radius',20);  
+    if strcmp(method,'Spread')
+        colormap(bluewhitered)
+    else
+        colormap(jet)
+    end
+    
+    gifname = [ Exp_ID '_' method '_' Var '.gif'];
+    n = 0;
+    for i = 1:N_periods
+        for j = 1:Duration
+            n = (i-1)*Duration +j;
+            t = dates(n)
+            data_dir = [ simul_dir '/date' num2str(i) ];
+            filename = ['Moorings_' num2str(year(t)) 'd' num2str(day(t,'dayofyear'),'%03d') '.nc']
+            clear data
+            if check_a_member==0
+                id = 1:Ne;
+            else
+                id = check_a_member;
+            end
+            for ie = id
+                file_dir = [data_dir '/mem' num2str(ie) '/' filename]
+    %             ncdisp(file_dir)
+                if strcmp(Var,'SIC')
+                    data_tmp = ncread(file_dir,'sic');
+                else
+                    data_tmp = ncread(file_dir,'sit');
+                end
+                data(ie,:,:) = data_tmp(:,:,1);
+            end   
+    %         data(data==0) = nan;   % exclude open water from nextsim.Moorings
+            if check_a_member>0
+                X = squeeze(data);              
+            else 
+                if strcmp(method,'Spread')
+                    X = squeeze(std(data,1));
+                elseif strcmp(method,'mean')==1
+                    X = squeeze(mean(data,1)); 
+                end
+            end
+            
+            % -------- plot ------------------------------     
+            lon = ncread(file_dir,'longitude');
+            lat = ncread(file_dir,'latitude');
+            
+            m_pcolor(lon,lat,X); shading flat; 
+            h=colorbar;
+            if strcmp(method,'mean')
+                if strcmp(Var,'SIC')
+                    caxis([0 1])
+                elseif strcmp(Var,'SIT')
+                    caxis([0 6]);
+                    title(h,'(m)')
+                end
+            else
+                if strcmp(Var,'SIC')
+                    caxis([0 0.5])
+                elseif strcmp(Var,'SIT')
+                    caxis([0 1.8]);
+                    title(h,'(m)')
+                end
+            end
+            
+            m_grid('color','k'); % 'linestyle','-'
+            m_coast('patch',0.7*[1 1 1]);  
+            title([datestr(t) ' ' method ' ' Var],'fontweight','normal','HorizontalAlignment','right');
+            set(findall(gcf,'-property','FontSize'),'FontSize',18);
+            
+            % -------- animation -------------------------------------
+            f = getframe(gcf);
+            im=frame2im(f);
+            [I,map] = rgb2ind(im,256);
+            gifname
+            if i==1  
+                imwrite(I,map,gifname,'gif','loopcount',inf,'Delaytime',.5)
+    %             imwrite(I,map,gifname,'gif');
+            else
+                imwrite(I,map,gifname,'gif','writemode','append','Delaytime',.5)
+            end
+            % ---------------------------------------------------------
+            X = reshape(X,1,[]);
+            meanspread(n) = nanmean(X);
+        end
+    end
+%     if check_a_member>0
+%         saveas(figure(1),[ Var '_mem' num2str(check_a_member) '.png'],'png')
+%     end
+    
+    figure(2)
+    set(figure(2),'Position',[100,200,550,400], 'color','w')
+    plot(dates, meanspread,'.-');
+    if strcmp(Var,'SIT')
+        ylabel([method ' of ' Var '(m)'])
+    else
+        ylabel([method ' of ' Var])
+    end
+
+%     axis([dates(1)-1 dates(end)+1 0 0.2]);
+    set(findall(gcf,'-property','FontSize'),'FontSize',18); 
+    saveas(figure(2),['Spatial_' method '_' Var '_main_moorings_spinup.png'],'png')
+end
+
+
+
+% convergen of filter. 
+% spread. 
+% srf.  spread reduction 1 is the devision by 2, 
+% dfs.  <=30
+% R reflexision,     a matrix of ensemble mean
+% square root filter apply inflation on anormalies
