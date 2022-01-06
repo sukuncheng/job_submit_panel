962887cf0bfbbe8223676f6fd74c00826d6a9181
sukuncheng <sukun.cheng@nersc.no>
2022-01-06 18:06:50 +0100

    update mooring_diagnostic code

--- 2020DA_scripts/analysis_matlab/main_moorings_diagnostic.m
+++ 2020DA_scripts/analysis_matlab/main_moorings_diagnostic.m
@@ -4,98 +4,55 @@ function [] = main_moorings_animation()
     close all
     % dbstop if error
     
-    main_settings
-    load('test_inform.mat')   % 
-    method = 'mean'; %{'mean', 'Spread'}  %mean: ensemble ensemble, spread: ensemble spread
-    N_contour = 20;
-    % simul_dir = '/cluster/work/users/chengsukun/simulations/test_FreeRun_2019-10-18_7days_x_26cycles_memsize40_offline_perturbations';
-    data_dir = [ simul_dir '/date11' ];
-    t = datetime(2019,12,31);
+    Ne = 40; % members   
+    mnt_dir = '/cluster/work/users/chengsukun/simulations'
+    % simul_dir = [ mnt_dir '/test_sic7sit7_2019-10-18_7days_x_26cycles_memsize40_d5'];
+    % simul_dir = [ mnt_dir '/test_FreeRun_2019-10-18_7days_x_26cycles_memsize40_OceanNudgingDd5'];
+    simul_dir = [ mnt_dir '/test_sic7_2019-10-18_7days_x_26cycles_memsize40_d5'];
+    data_dir  = [ simul_dir '/date1' ];
+    t = datetime(2019,10,24);
     filename = ['Moorings_' num2str(year(t)) 'd' num2str(day(t,'dayofyear'),'%03d') '.nc'];
-    check_a_member = 1; % check_a_member=0 presents ensemble average
-    if check_a_member==0
-        id = 1:Ne;
-    else
-        id = check_a_member;
-    end    
-    for ie = id
-        file_dir = [data_dir '/mem' num2str(ie) '/' filename]
+
+    for ie = 1:Ne
+        file_dir = [data_dir '/mem' num2str(ie) '/' filename];
         data_tmp = ncread(file_dir,'sic');
         sic(ie,:,:) = data_tmp(:,:,1);
         data_tmp = ncread(file_dir,'sit');
-        sit_sic(ie,:,:) = data_tmp(:,:,1);
-        
+        sit_sic(ie,:,:) = data_tmp(:,:,1);    
     end   
-    sit_sic(sic==0) = nan;   % exclude open water from nextsim.Moorings
-    sic(sic==0)=nan;
+    % sic(sic<0.15) = 0;
+    sit_sic(sic<1.e-6) = nan;
+    sic(sic<1.e-6) = nan;
     sit = sit_sic./(sic+1.e-10);
-    if check_a_member>0
-        SIC = squeeze(sic); 
-        SIT = squeeze(sit);
-        SIT_SIC = squeeze(sit_sic);               
-    else 
-        SIC = squeeze(mean(sic,1)); 
-        SIT = squeeze(mean(sit,1));
-        SIT_SIC = squeeze(mean(sit_sic,1));               
-    end
-
     lon = ncread(file_dir,'longitude');
     lat = ncread(file_dir,'latitude');
-    
+    for i = 1:size(lon,1)
+        for j = 1:size(lat,2)
+            sicsit_corr(i,j) = corr(squeeze(sic(:,i,j)),squeeze(sit(:,i,j)));
+        end
+    end    
     % -------- plot ------------------------------     
-    figure(1);set(gcf,'Position',[10,15,1800,800], 'color','w')
-    colormap(viridis)
-    m_proj('Stereographic','lon',-45,'lat',90,'radius',25); 
-    subplot(131)
-    Var = SIC;
-    m_contourf(lon,lat,Var,N_contour); shading flat
-    % m_scatter(reshape(lon,1,[]),reshape(lat,1,[]),[],reshape(Var,1,[]),'.');
-    h = colorbar;
-    % title(h,'(m)')  
-    m_coast('patch',0.7*[1 1 1]);  hold on;
-    m_grid('color','k');  
-    set(gca,'XTickLabel',[],'YTickLabel',[]);
-    caxis([0 1])
-    title({[Exp_ID ' SIC'],''})
-    
-    subplot(132)
-    Var = SIT;
-    m_contourf(lon,lat,Var,N_contour); shading flat
-    % m_scatter(reshape(lon,1,[]),reshape(lat,1,[]),[],reshape(Var,1,[]),'.');
-    h = colorbar;
-    title(h,'(m)')  
-    m_coast('patch',0.7*[1 1 1]);  hold on;
-    m_grid('color','k');  
-    % caxis([0 5])
-    set(gca,'XTickLabel',[],'YTickLabel',[]);
-    title({['SIT mean:' num2str(nanmean(nanmean(Var)))],''})
-    
-    
-    subplot(133)
-    Var = SIT_SIC;
-    m_contourf(lon,lat,Var,N_contour); shading flat
-    % m_contourf(lon(:,:,1),lat(:,:,1),fice(:,:,1),N_contour); shading flat;
-    % m_scatter(reshape(lon,1,[]),reshape(lat,1,[]),[],reshape(Var,1,[]),'.');
-    h = colorbar;1
-    title(h,'(m)')  
-    m_coast('patch',0.7*[1 1 1]);  hold on;
-    m_grid('color','k');  
-    set(gca,'XTickLabel',[],'YTickLabel',[]);
-    % caxis([0 5])
-    title({['SIT*SIC  mean:' num2str(nanmean(nanmean(Var)))],''})
-    
-    set(findall(gcf,'-property','FontSize'),'FontSize',15);
+    figure(1);set(gcf,'Position',[10,15,800,600], 'color','w')   
+    fun_geo_pcolor(lon,lat, sicsit_corr,'sic-sit\_corr',''); 
+    colormap(gca,bluewhitered);
+    set(findall(gcf,'-property','FontSize'),'FontSize',12);
 
-    Exp_ID = 'FreeRun';
-    if check_a_member>0
-        saveas(figure(1),[ Exp_ID '_2D_mem' num2str(check_a_member) '.png'],'png')
-    else
-        saveas(figure(1),[ Exp_ID '_2D_ensmean.png'],'png')
-    end
+    saveas(figure(1),[ 'sic_sit_corr.png'],'png')
           
 end
 
 
+function fun_geo_pcolor(lon,lat,Var,Title, unit)
+    Var(Var==0) = nan;
+    m_proj('Stereographic','lon',-45,'lat',90,'radius',20);
+    m_pcolor(lon, lat, Var); shading flat; 
+    m_coast('patch',0.7*[1 1 1]);    
+    set(gca,'XTickLabel',[],'YTickLabel',[]);
+    title({Title,''},'fontweight','normal')
+    m_grid('linest',':');
+    h = colorbar%('southoutside');
+    title(h, unit);
+end
 
 % convergen of filter. 
 % spread. 
